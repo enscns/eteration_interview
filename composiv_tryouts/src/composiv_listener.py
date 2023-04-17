431a85e93294fe891b75f94d2139d856d9c4fd85
Enes <Enes Cansu>
2023-04-18 01:40:06 +0300

    last update

--- composiv_tryouts/src/composiv_listener.py
+++ composiv_tryouts/src/composiv_listener.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import rospy
-from std_msgs.msg import String
+import rospy                        # This is the python library in ROS environment
+from std_msgs.msg import String     # This is the type of message
+
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + " I heard %s ", data.data)
+    rospy.loginfo(rospy.get_caller_id() + " I heard %s ", data.data) 
     
 def listener():
 
@@ -13,9 +14,13 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+    # this line declares that the node name is 'listener'.
+    rospy.init_node('listener', anonymous=True)   
+
+    # this line declares that the node subscribing to chatter topic and if the 
+    # std_msgs.msg.String type messages is received the callback functions are invoked.
+    rospy.Subscriber("chatter", String, callback) 
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- composiv_tryouts/src/composiv_talker.py
+++ composiv_tryouts/src/composiv_talker.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
-import rospy
-from std_msgs.msg import String
+import rospy                        # This is the python library in ROS environment
+from std_msgs.msg import String     # This is the type of message
 
 def talker():
+
+    # this line declares that the node publishing to chatter topic using the 
+    # message type String which is actually the class std_msgs.msg.String.
     pub = rospy.Publisher('chatter', String, queue_size=10)
+
+    # this line declares that the node name is 'talker'
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(2) # 2hz
+
+    rate = rospy.Rate(1) # 1hz
+
+    #This while loop tells that these messages will be transmitted in a certain 
+    #loop continuously, unless there is an external shutdown command.
     while not rospy.is_shutdown():
         hello_str = " hello world "
         rospy.loginfo(rospy.get_caller_id() + hello_str)
